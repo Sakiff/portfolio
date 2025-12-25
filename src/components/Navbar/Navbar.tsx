56e41556daabe45249228368460bc864afd9519e
Sakiff <ftliyevsakif@gmail.com>
2025-12-25 19:13:41 +0400

    added homepage

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,9 +7,14 @@ import { ModeToggle } from "../Theme/toggle";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [rotating, setRotating] = useState(false);
 
   const openMenu = (): void => {
-    setMenuOpen(!menuOpen);
+    setRotating(true);
+    setTimeout(() => {
+      setMenuOpen((prev) => !prev);
+      setRotating(false);
+    }, 250);
   };
 
   useEffect(() => {
@@ -29,12 +34,12 @@ const Navbar = () => {
       <div className=" w-full xl:w-11/12 relative">
         <div
           className={` ${
-            menuOpen ? "h-[100vw]" : "h-20"
+            menuOpen ? "h-[calc(100vw+100px)]" : "h-20"
           } border-5 border-border rounded-xl transition-all duration-300 ease-in-out `}
         >
           <div
             className={`${
-              menuOpen ? "h-[100vw]" : "h-20"
+              menuOpen ? "h-[calc(100vw+100px)]" : "h-20"
             } w-full border-5 border-border rounded-xl absolute top-3 left-3 transition-all duration-300 ease-in-out `}
           >
             <div className="flex items-center justify-between pt-2 px-10 md:justify-around md:px-0 ">
@@ -42,7 +47,7 @@ const Navbar = () => {
               <div className="flex items-center gap-5">
                 <Link to={"/"}>
                   <h1 className="text-3xl">
-                    Sakif <strong className="text-muted-foreground">.</strong>
+                    Sakif <strong className="text-amber-600">.</strong>
                   </h1>
                 </Link>
                 <div className="flex md:hidden pt-1">
@@ -52,15 +57,13 @@ const Navbar = () => {
 
               {/* Bars Menu Icon */}
 
-              <div className="pt-1">
-                {menuOpen ? (
-                  <X className="cursor-pointer" onClick={() => openMenu()} />
-                ) : (
-                  <Menu
-                    className="flex md:hidden cursor-pointer"
-                    onClick={() => openMenu()}
-                  />
-                )}
+              <div
+                onClick={openMenu}
+                className={`visible md:invisible cursor-pointer transition-all duration-300 ease-in-out pt-1 ${
+                  rotating ? "opacity-0 -rotate-360" : "rotate-0 opacity-100"
+                }`}
+              >
+                {menuOpen ? <X /> : <Menu />}
               </div>
 
               {/* Desktop Menu */}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,70 @@
+import { Button } from "@/components/ui/button";
+import {
+  Download,
+  Github,
+  Instagram,
+  Linkedin,
+  type LucideIcon,
+} from "lucide-react";
+
+import styles from "./HomePage.module.css";
+
+type SocialLinkIcon = {
+  path: string;
+  icon: LucideIcon;
+};
+
+const SOCIAL_LINKS: readonly SocialLinkIcon[] = [
+  { path: "https://github.com/Sakiff", icon: Github },
+  {
+    path: "https://www.linkedin.com/in/sakif-fataliyev-38b460370/",
+    icon: Linkedin,
+  },
+  { path: "https://www.instagram.com/sakif.ftlyv/", icon: Instagram },
+];
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center pt-20">
+      <div className="flex items-center justify-around pt-10 w-11/12 flex-col lg:flex-row gap-5 lg:gap-0 ">
+        {/* Hero Text (Left Side) */}
+        <div className="flex items-center lg:items-start  flex-col gap-3 lg:gap-2 w-full lg:w-1/3">
+          <span className="text-xl">MERN Stack Developer</span>
+          <strong className={styles.fontSize}>Hello I'm</strong>
+          <strong className={` text-amber-700 ${styles.fontSize}`}>
+            Fataliyev Sakif
+          </strong>
+          <p className="text-lg text-center lg:text-start">
+            I craft digital experiences that feel simple, smooth, and alive.
+            Where creativity meets clean and purposeful code.
+          </p>
+
+          <div className="flex items-center justify-center gap-3">
+            <Button variant={"outline"} className="uppercase cursor-pointer">
+              Download CV <Download />
+            </Button>
+
+            {SOCIAL_LINKS.map(({ path, icon: Icon }) => (
+              <a href={path} target="_blank" key={path}>
+                <Button variant={"outline"} className="cursor-pointer">
+                  <Icon />
+                </Button>
+              </a>
+            ))}
+          </div>
+        </div>
+
+        {/* Hero Section Photo (Right Side) */}
+        <div className={`border-2 ${styles.borderAnimation}`}>
+          <img
+            src="/assets/hero_image.jpeg"
+            alt="profile_photo"
+            className="w-full h-full "
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
