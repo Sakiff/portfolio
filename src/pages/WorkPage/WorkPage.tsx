2df1594272ea1c0444bba4c1302e28b588e16df5
Sakiff <ftliyevsakif@gmail.com>
2026-01-21 16:16:58 +0400

    fixed gap in works page in mobile view

--- src/pages/WorkPage/WorkPage.tsx
+++ src/pages/WorkPage/WorkPage.tsx
@@ -3,7 +3,7 @@ import { WORK_CARDS } from "@/data/workCards";
 
 const WorkPage = () => {
   return (
-    <div className="pt-20 px-8 pb-8">
+    <div className="pt-20 lg:px-8 pb-8">
       <div className="max-w-7xl mx-auto">
         <h1 className="text-4xl font-bold text-center mb-12">My Works</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
